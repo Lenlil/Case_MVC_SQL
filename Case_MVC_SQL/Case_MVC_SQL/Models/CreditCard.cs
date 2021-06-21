b8e75d87bcb480b4984a0a9fde3447535b5aa462
Lenlil <55378996+Lenlil@users.noreply.github.com>
2021-06-21 16:59:02 +0200

    DB set up with Entity Framework

---
+++ Case_MVC_SQL/Case_MVC_SQL/Models/CreditCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Case_MVC_SQL.Models
+{
+    public partial class CreditCard
+    {
+        public int CreditCardId { get; set; }
+        public string CardType { get; set; }
+        public string CardNumber { get; set; }
+        public byte ExpMonth { get; set; }
+        public short ExpYear { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ Case_MVC_SQL/Case_MVC_SQL/Models/CreditCardsContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Case_MVC_SQL.Models
+{
+    public partial class CreditCardsContext : DbContext
+    {
+        public CreditCardsContext()
+        {
+        }
+
+        public CreditCardsContext(DbContextOptions<CreditCardsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CreditCard> CreditCards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost; Database=CreditCards ;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<CreditCard>(entity =>
+            {
+                entity.ToTable("CreditCard", "Sales");
+
+                entity.Property(e => e.CreditCardId).HasColumnName("CreditCardID");
+
+                entity.Property(e => e.CardNumber)
+                    .IsRequired()
+                    .HasMaxLength(25);
+
+                entity.Property(e => e.CardType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
